e6e36303610184fcac0dfd0d2dcb4c3c48d9b525
hughjayyu <hugh-jayyu2023@u.northwestern.edu>
2023-09-13 21:58:47 -0500

    hmm, dist?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  // ...other config options
+	buildDir: 'dist',
+
   devtools: { enabled: true },
    server: {
 		middleware: {
