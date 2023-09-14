995935752d931344d8a9466356e80873d48d6a6e
hughjayyu <hugh-jayyu2023@u.northwestern.edu>
2023-09-13 23:31:03 -0500

    Hour like 13 man

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const port = process.env.PORT || 8081; // Use process.env.PORT for flexibility
 
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
-app.use('/', serveStatic(path.join(__dirname, 'dist')));
 
 // Middleware to handle API routes
 app.use((req, res, next) => {
@@ -25,7 +24,7 @@ app.use((req, res, next) => {
     next();
   } else {
     // Request is for the front-end, serve the HTML file
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '.output/server', 'index.mjs'));
   }
 });
 // Open a connection to your SQLite3 database
