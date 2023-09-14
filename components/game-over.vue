97d7b25aae7c98081c6fd1f250e6a224439fcce1
hughjayyu <hugh-jayyu2023@u.northwestern.edu>
2023-09-13 20:42:30 -0500

    r Please enter the commit message for your changes. Lines starting

--- components/game-over.vue
+++ components/game-over.vue
@@ -101,7 +101,7 @@ export default {
   methods: {
      async fetchGlobalStats() {
       try {
-        const response = await fetch('http://localhost:8081/api/global-stats'); // Replace with your actual API endpoint
+        const response = await fetch('https://jvgrid.com/api/global-stats'); // Replace with your actual API endpoint
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
