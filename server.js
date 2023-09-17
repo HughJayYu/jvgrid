0b14ce9c85e42489833f20f691fe3bb8cbb3fefc
hughjayyu <hugh-jayyu2023@u.northwestern.edu>
2023-09-17 10:33:15 -0500

    server mistake oopsies

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use('/api', searchRoute);
 let storedBoardData = null;
 let boardID = 6;
 //BOARD GENERATION COMMENT STARTS HERE
-/*
 const db3 = new sqlite3.Database('game_boards.db');
 db3.get('SELECT * from daily_data WHERE day = 5', [], (err, row) => {
 	if (err) { 
@@ -99,7 +98,7 @@ app.get('/api/rarity-scores', (req, res) => {
 
 // Serve Nuxt 3 frontend
 app.use(express.static(path.join(__dirname, '.output/client')));
-*/
+
 //BOARD GENERATION CLOSE COMMENT GOES HERE
 // Start the server
 app.listen(port, () => {
